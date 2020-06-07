9e678193a8a8ceb4be3f5a0e5a0e0df750d35d9b
Ventrix94 <venturinipatrick@aol.com>
2020-06-07 18:53:54 +0200

    New availability Bug-fix; Update delete-Calls of resources and users

--- ReactApp/src/components/administrationComponents/userComponents/UserRequests.js
+++ ReactApp/src/components/administrationComponents/userComponents/UserRequests.js
@@ -11,13 +11,13 @@ export const addEmployee = data => axios.post(`${APIURL}/api/Employees/`, data)
 export const updateEmployee = (userID,data) => axios.put(`${APIURL}/api/Employees/${userID}`, data) 
 export const getEmployeeList = () => axios.get(`${APIURL}/api/Employees/`) 
 export const getEmployee = userID => axios.get(`${APIURL}/api/Employees/${userID}`) 
-//export const deleteEmployee = userID => axios.delete(`${APIURL}/api/Employees/${userID}`)
+export const deleteEmployee = userID => axios.delete(`${APIURL}/api/Employees/${userID}`)
 //Customers
 export const addCustomer = data => axios.post(`${APIURL}/api/Customers/`, data) 
 export const updateCustomer = (userID,data) => axios.put(`${APIURL}/api/Customers/${userID}`, data) 
 export const getCustomerList = () => axios.get(`${APIURL}/api/Customers/`) 
 export const getCustomer = userID => axios.get(`${APIURL}/api/Customers/${userID}`) 
-//export const deleteCustomer = userID => axios.delete(`${APIURL}/api/Customers/${userID}`)
+export const deleteCustomer = userID => axios.delete(`${APIURL}/api/Customers/${userID}`)
 
 //USERS
 export const getAllUsers = () => axios.get(`${APIURL}/api/Users`) 
