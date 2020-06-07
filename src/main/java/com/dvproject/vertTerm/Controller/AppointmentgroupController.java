0b3a6c918114c2931017c7b624aa98bc5ededa7f
JoshuaNoelMueller <joshua.noel.mueller97@gmail.com>
2020-06-07 23:39:24 +0200

    Test commit, so I may know what the booking process does wrong

--- src/main/java/com/dvproject/vertTerm/Controller/AppointmentgroupController.java
+++ src/main/java/com/dvproject/vertTerm/Controller/AppointmentgroupController.java
@@ -68,7 +68,12 @@ public class AppointmentgroupController {
 	public boolean bookAppointments (
 			@PathVariable String userid, 
 			@RequestBody Appointmentgroup appointmentgroup) {
-		return appointmentgroupService.bookAppointmentgroup(userid, appointmentgroup);
+		try {
+			return appointmentgroupService.bookAppointmentgroup(userid, appointmentgroup);
+		} catch (RuntimeException ex) {
+			ex.printStackTrace();
+			return false;
+		}
 	}
 	
 	@PutMapping("")
