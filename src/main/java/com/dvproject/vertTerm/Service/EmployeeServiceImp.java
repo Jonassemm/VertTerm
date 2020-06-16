152ec58b26b66bf3116f538efe2988030bf8f38e
JoshuaNoelMueller <joshua.noel.mueller97@gmail.com>
2020-06-16 17:24:02 +0200

    Fixed a bug in the employeeservice where a NullpointerException was thrown

--- src/main/java/com/dvproject/vertTerm/Service/EmployeeServiceImp.java
+++ src/main/java/com/dvproject/vertTerm/Service/EmployeeServiceImp.java
@@ -52,14 +52,17 @@ public class EmployeeServiceImp implements EmployeeService, AvailabilityService 
     }
 
     public List<Employee> getAll(String positionId){
-        List<Employee> result= new ArrayList<>();
-        for(Employee employee : repo.findAll()){
-            for (Position position : employee.getPositions())
-            if(position.getId().equals(positionId)) {
-                result.add(employee);
-            }
-        }
-        return result;
+//        List<Employee> result= new ArrayList<>();
+//        for(Employee employee : repo.findAll()){
+//            for (Position position : employee.getPositions())
+//            if(position.getId().equals(positionId)) {
+//                result.add(employee);
+//            }
+//        }
+//        return result;
+        
+        return repo.findByPositionsId(positionId);
+        
     }
 
     @Override
