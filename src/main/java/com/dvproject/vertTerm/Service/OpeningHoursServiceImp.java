f0b985893c07a2d72b9ceaf7365fbad0a5b42e6d
JoshuaNoelMueller <joshua.noel.mueller97@gmail.com>
2020-06-26 23:32:27 +0200

    Refactored OpeningHoursServiceImp to be less complex

--- src/main/java/com/dvproject/vertTerm/Service/OpeningHoursServiceImp.java
+++ src/main/java/com/dvproject/vertTerm/Service/OpeningHoursServiceImp.java
@@ -1,29 +1,28 @@
 package com.dvproject.vertTerm.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dvproject.vertTerm.Model.Availability;
 import com.dvproject.vertTerm.Model.OpeningHours;
-import com.dvproject.vertTerm.repository.AvailabilityRepository;
 import com.dvproject.vertTerm.repository.OpeningHoursRepository;
 
 @Service
 public class OpeningHoursServiceImp implements OpeningHoursService {
 	@Autowired
-	private OpeningHoursRepository repo;
-
-	@Autowired
-	private AvailabilityRepository availRepo;
+	private OpeningHoursRepository openingHoursRepository;
 
 	@Override
 	public OpeningHours get() {
-		List<OpeningHours> openingHours = repo.findAll();
+		List<OpeningHours> openingHours = openingHoursRepository.findAll();
 
 		if (openingHours == null || openingHours.size() == 0)
 			throw new RuntimeException("Two OpeningHourshave not been setup correctly, please restart the webapp");
@@ -31,59 +30,36 @@ public class OpeningHoursServiceImp implements OpeningHoursService {
 		if (openingHours.size() > 1)
 			throw new RuntimeException("Two OpeningHours objects in database!");
 
-		return repo.findAll().get(0);
+		return openingHoursRepository.findAll().get(0);
 	}
 
 	@Override
 	public OpeningHours update(OpeningHours updatedInstance) {
-		List<Availability> updatedInstanceAvailability = updatedInstance.getAvailabilities();
-		updateInteral(updatedInstanceAvailability);
-		
+		List<Availability> availabilitiesToInsert = new ArrayList<>();
+		Map<String, Availability> updatedAvailabilities = new HashMap<>();
 		OpeningHours openingHours = get();
 		List<Availability> openingHoursAvailabilities = openingHours.getAvailabilities();
-		Map<String, Availability> availabilitiesMap;
 
-		// db-object: id -> Availability
-		availabilitiesMap = openingHoursAvailabilities != null
-				? openingHoursAvailabilities.stream()
-						.collect(Collectors.toMap(Availability::getId, availability -> availability))
-				: null;
-
-		// add all new Availabilities 
-		if (availabilitiesMap != null)
-			openingHoursAvailabilities.addAll(updatedInstanceAvailability.stream()
-					.filter(avail -> !availabilitiesMap.containsKey(avail.getId())).collect(Collectors.toList()));
-		else 
-			openingHours.setAvailabilities(updatedInstanceAvailability);
-
-		repo.save(openingHours);
-
-		return get();
-	}
-
-	public void updateInteral(List<Availability> availabilities) {
-		Availability repoAvailability = null;
-
-		for (int i = 0; i < availabilities.size(); i++) {
-			Availability availability = availabilities.get(i);
-
-			if (availability.getId() != null) {
-				repoAvailability = availRepo.findById(availability.getId()).orElse(null);
-				if (repoAvailability != null) {
-					Date endOfSeries = availability.getEndOfSeries();
-					// endOfSeries should be set
-					if (repoAvailability.getEndOfSeries() == null && endOfSeries != null) {
-						repoAvailability.setEndOfSeries(endOfSeries);
-						availRepo.save(repoAvailability);
-						availabilities.set(i, repoAvailability);
-					}
-				} else { // id != null && no entity in db
-					availability.setId(null);
-					repoAvailability = availRepo.save(availability);
-				}
-			} else // id == null
-				repoAvailability = availRepo.save(availability);
+		for (Availability availability : updatedInstance.getAvailabilities()) {
+			if (availability.getId() == null) {
+				availability.setId(new ObjectId ().toString());
+				availabilitiesToInsert.add(availability);
+			} else
+				updatedAvailabilities.put(availability.getId(), availability);
 		}
+
+		for (Availability availability : openingHoursAvailabilities) {
+			Availability updatedAvailability = updatedAvailabilities.get(availability.getId());
+			if (updatedAvailability != null && availability.getEndOfSeries() == null
+					&& updatedAvailability.getEndOfSeries() != null)
+				availability.setEndOfSeries(updatedAvailability.getEndOfSeries());
+		}
+		
+		openingHoursAvailabilities.addAll(availabilitiesToInsert);
+		
+		openingHoursRepository.save(openingHours);
+		
+		return get();
 	}
 
 	@Override
@@ -96,7 +72,7 @@ public class OpeningHoursServiceImp implements OpeningHoursService {
 				.filter(avail -> avail.getEndOfSeries() == null || avail.getEndOfSeries().after(now))
 				.collect(Collectors.toList()));
 
-		repo.save(openingHours);
+		openingHoursRepository.save(openingHours);
 	}
 
 }
